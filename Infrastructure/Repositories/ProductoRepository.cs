f9863187a52b540493015faab885990e0281cef6
dguia18 <dguia@unicesar.edu.co>
2020-05-02 23:40:52 -0500

    Base infraestructura

---
+++ Infrastructure/Repositories/ProductoRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain;
+using Domain.Repositories;
+using Infrastructure.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Repositories
+{
+    public class ProductoRepository : GenericRepository<Producto>, IProductoRepository
+    {
+        public ProductoRepository(IDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ Infrastructure/TiendaContext.cs
@@ -0,0 +1,13 @@
+﻿using Infrastructure.Base;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Infrastructure
+{
+    public class TiendaContext : DbContextBase
+    {
+        public TiendaContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
